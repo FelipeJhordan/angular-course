6681e1c84903c8ab60d107d8ac40ab4d4d8b12a0
Felipe Jhordan <44248690+FelipeJhordan@users.noreply.github.com>
2023-10-11 14:40:21 -0300

    feat: add all register validations and create alert component

--- clips/src/app/user/register/register.component.ts
+++ clips/src/app/user/register/register.component.ts
@@ -7,23 +7,49 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  showAlert = false
+  alertMsg = 'Please wait! Your account is being created.'
+  alertColor = 'blue'
+
   name=  new FormControl('', [
     Validators.required,     
     Validators.minLength(3) 
   ])
-  email=  new FormControl('')
-  age=  new FormControl('')
-  password=  new FormControl('')
-  confirm_password=  new FormControl('')
-  phoneNumber=  new FormControl('')
+  email=  new FormControl('', [
+    Validators.required,
+    Validators.email
+  ])
+  age=  new FormControl('', [
+    Validators.required,
+    Validators.min(18),
+    Validators.max(120)
+  ])
+  password=  new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ])
+  confirm_password=  new FormControl('', [ Validators.required])
+  phoneNumber=  new FormControl('', [
+    Validators.required,
+    Validators.minLength(13),
+    Validators.minLength(13),
+  ])
   registerForm = new FormGroup({
     name: this.name,
-    email: this.name,
-    age: this.email,
+    email: this.email,
+    age: this.age,
     password: this.password,
     confirm_password: this.confirm_password,
     phoneNumber: this.phoneNumber,
   })
+
+ 
+  register() {
+    this.showAlert = true
+    this.alertMsg = 'Please wait! Your account is being created.'
+    this.alertColor = 'blue'
+  }
 
 
 }
